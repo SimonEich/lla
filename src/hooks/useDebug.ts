bfc26e5805bc48cd97e43a5d68c644fec3813b18
Simon Eich <simon.eich1998@gmail.com>
2026-06-08 20:11:02 -0600

    playing with the list

--- src/hooks/useDebug.ts
+++ src/hooks/useDebug.ts
@@ -1,16 +1,20 @@
+import { Word } from "@/data/words";
 import { wordsService } from "@/services/wordsService";
 import { useState } from "react";
 
 export function useDebug() {
   const [ wordCount, setWordCount ] = useState<number>(0);
+  const [ firstWord, setFirstWord ] = useState('');
   async function runDebug() {
     console.log('[useDebug] start');
 
     const words = await wordsService.getAll();
     console.log('[useDebug] Anzahl Wörter:', words.length);
+    console.log('[useDebug] First Word', words[0].sentences[0]);
     setWordCount(words.length);
+    setFirstWord(words[0].native);
 
   }
 
-  return { runDebug, wordCount };
+  return { runDebug, wordCount, firstWord };
 }
