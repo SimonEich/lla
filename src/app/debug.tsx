f708e5d7db1f0fe7935324559ca03ba76d857e92
Simon Eich <simon.eich1998@gmail.com>
2026-06-16 19:00:48 -0600

    back to old state and many fixes

--- src/app/debug.tsx
+++ src/app/debug.tsx
@@ -1,15 +1,13 @@
-import { View } from 'react-native';
+import { View, Text} from 'react-native';
 import { useDebug } from '../hooks/useDebug';
 import { useSession } from '@/hooks/useSession';
 import { Debug } from '@/components/debugcard';
 
 export default function SessionDebugScreen() {
-  const { runDebug, data } = useDebug();
-  const { initSession, countActive } = useSession();
 
   return (
     <View style={{ flex: 1 }}>
-      <Debug runDebug={runDebug} data={data} runSession={initSession} countActive={countActive}/>
+      <Text>Debug modus</Text>
     </View>
   );
 }
--- src/app/learn.tsx
+++ src/app/learn.tsx
@@ -1,24 +1,33 @@
-import { View, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
 import { useSession } from '@/hooks/useSession';
 import { MultipleChoice } from '@/components/levels/MultipleChoice';
 
 export default function LearnScreen() {
-  const { data } = useSession();
-
-  if (!data) {
-    return <View style={styles.container} />;
-  }
-
   return (
-    <View style={styles.container}>
-      <MultipleChoice data={data} />
+    <View>
+      <Text>
+        Learn screen
+      </Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f5f5f5',
-  },
-});
+//  const { data } = useSession();
+//
+//  if (!data) {
+//    return <View style={styles.container} />;
+//  }
+//
+//  return (
+//    <View style={styles.container}>
+//      <MultipleChoice data={data} />
+//    </View>
+//  );
+//}
+//
+//const styles = StyleSheet.create({
+//  container: {
+//    flex: 1,
+//    backgroundColor: '#f5f5f5',
+//  },
+//});
